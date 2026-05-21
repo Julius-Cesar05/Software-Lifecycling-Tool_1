244966eaf4a9c5f0fa32e312d6b62ed891b507e7
lukas <wi25b088@technikum-wien.at>
2026-05-21 15:22:35 +0200

    Test wi25b088 Push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//Test wi25b088
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
