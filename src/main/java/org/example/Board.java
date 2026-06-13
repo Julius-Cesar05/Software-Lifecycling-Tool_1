ac9ef29ef837203fae59e01bf1960d8b3042256b
lukas <wi25b088@technikum-wien.at>
2026-06-13 15:38:24 +0200

    US-003 notify player when game ends

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -32,4 +32,19 @@ public class Board {
     public char[][] getCells() {
         return cells;
     }
+
+
+    public boolean isFull() {
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                if (cells[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
